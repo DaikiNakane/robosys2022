98ad5a9982c1ec4e4b206240ba5a267f3b895cbb
Daiki Nakane <nakane2501@gmail.com>
2023-03-05 22:31:39 +0900

    Add a test

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
-
-out=$(seq 5 | ./plus)
-
-[ "${out}" = 14 ]
+#PDX-FileCopyrightText: 2023 Daiki Nakane
+# SPDX-License-Identifier: BSD-3-Clause
+　
+ng () {
+	        echo NG at Line $1
+		        res=1
+		}
+		　
+		res=0
+		　
+		### I/O TEST ###
+		out=$(seq 5 | ./plus)
+		[ "${out}" = 15 ] || ng ${LINENO}
+		　
+		[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
+		exit $res
